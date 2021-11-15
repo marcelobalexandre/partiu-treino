01bf4abb5ff861ab5216f9993944127194f13593
Marcelo Alexandre <marcelobalexandre@gmail.com>
2021-11-15 17:06:33 -0300

    Add Material UI

---
+++ web/pages/_document.tsx
@@ -0,0 +1,29 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+
+class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default Document
